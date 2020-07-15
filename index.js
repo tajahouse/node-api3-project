2a5dd2c61e7a6b849bc3714e9970fb19d9177309
Taja House <ahlliahtaja@yahoo.com>
2020-07-15 13:43:17 -0700

    Completed MVP

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code away!
+const express = require('express');
+const server = express();
+const port  = 5000
+
+server.use(express.json());
+
+server.get('/', (req, res)=>{
+    res.send(`Stay strong Taja`)
+})
+
+server.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
+})
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+const UserRouter = require("./users/userRouter");
+const PostRouter = require("./posts/postRouter");
 
-server.get('/', (req, res) => {
+server.use(logger);
+server.use(express.json());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+server.use("/api/users", UserRouter);
+server.use("/api/posts", PostRouter);
 
-function logger(req, res, next) {}
-
-module.exports = server;
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl} at ${new Date().toString()}`);
+  next();
+}
